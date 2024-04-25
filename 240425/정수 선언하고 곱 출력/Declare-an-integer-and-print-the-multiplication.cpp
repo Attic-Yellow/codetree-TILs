5fbd47d6c196b71b8fe331bcac96a65d8f870680
DongHee Lee <117146643+Attic-Yellow@users.noreply.github.com>
2024-04-25 21:10:26 +0900

    Title: 정수 선언하고 곱 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240425/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a = 0;
+    int b = 0;
+
+    a = 26;
+    b = 5;
+
+    cout << a << " * " << b << " = " << (a * b);
+    return 0;
+}
