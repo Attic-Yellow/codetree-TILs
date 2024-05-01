ce804fe2a5d3f28170e9dcae2f61d89a4e128435
DongHee Lee <117146643+Attic-Yellow@users.noreply.github.com>
2024-05-01 13:15:16 +0900

    Title: 짝수만 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240501/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a, b;
+
+    cin >> b >> a;
+
+    int i = b;
+
+    while (i >= a)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << " ";
+        }
+        i--;
+    }
+
+    return 0;
+}
