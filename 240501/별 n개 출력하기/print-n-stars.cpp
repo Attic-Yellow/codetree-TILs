4303960e67aea0efe93f14e058f2e02a783511cc
DongHee Lee <117146643+Attic-Yellow@users.noreply.github.com>
2024-05-01 13:11:16 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240501/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    int i;
+    while (i < n)
+    {
+        cout << "*" << " \n";
+        i++;
+    }
+
+    return 0;
+}
